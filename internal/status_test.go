f6506be162d4db6100dafd663a1b7ccc6e6bf241
Dylan Williams <mdylanwilliams576@gmail.com>
2024-11-04 00:04:48 -0500

    Added and reorganized unit tests
    status_test.go now has TestSLWith2Resources and TestSLWith1000Resources. Each of these make use of the utility function createResoures.
    
    The go test sometimes fails due to the bug that's already been found--the SL sometimes doesn't do its last update.
    This bug needs fixed, and more unit tests need added, including testing context cancellation.
    
    Signed-off-by: Dylan Williams <mdylanwilliams576@gmail.com>

--- internal/status_test.go
+++ internal/status_test.go
@@ -10,39 +10,65 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetStatusString(t *testing.T) {
-	// Test 1000 resources.
+func TestSLWith2Resources(t *testing.T) {
+	// Create and init new SL with generate resources.
+	resources := createResources(2, t)
+	ctx, _ := context.WithCancel(context.Background())
+	st1 := NewStatusLine(ctx, &resources)
+	err := st1.InitResourcesSizes(1)
+	assert.NotNil(t, err)
+	err = st1.InitResourcesSizes(1000)
+	assert.Nil(t, err)
+
+	st1.Start(false)
+	assert.Equal(t, "\r-[            ]          0/2 Resources          0 B / 12 B          0s elapsed", st1.GetStatusString())
+	st1.Increment(0)
+	time.Sleep(10 * time.Millisecond) // Give SL loop time to update fields.
+	assert.Equal(t, "\r\\[██████      ]          1/2 Resources          6 B / 12 B          0s elapsed", st1.GetStatusString())
+	st1.Increment(1)
+	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, "\r✔[████████████]          2/2 Resources          12 B / 12 B          0s elapsed", st1.GetStatusString())
+
+}
+
+func TestSLWith1000Resources(t *testing.T) {
+	resources := createResources(1000, t)
+	ctx, _ := context.WithCancel(context.Background())
+	st2 := NewStatusLine(ctx, &resources)
+	err := st2.InitResourcesSizes(1)
+	assert.NotNil(t, err)
+	err = st2.InitResourcesSizes(1000)
+	assert.Nil(t, err)
+
+	st2.Start(false)
+	assert.Equal(t, "\r-[                    ]          0/1000 Resources          0 B / 6.0 kB          0s elapsed", st2.GetStatusString())
+	st2.Increment(0)
+	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, "\r\\[                    ]          1/1000 Resources          6 B / 6.0 kB          0s elapsed", st2.GetStatusString())
+	st2.Increment(1)
+	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, "\r|[                    ]          2/1000 Resources          12 B / 6.0 kB          0s elapsed", st2.GetStatusString())
+	st2.Increment(2)
+	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, "\r/[                    ]          3/1000 Resources          18 B / 6.0 kB          0s elapsed", st2.GetStatusString())
+	st2.Increment(3)
+	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, "\r-[                    ]          4/1000 Resources          24 B / 6.0 kB          0s elapsed", st2.GetStatusString())
+	for i := 4; i < 1000; i++ {
+		st2.Increment(i)
+	}
+	assert.Equal(t, "\r✔[████████████████████]          1000/1000 Resources          6.0 kB / 6.0 kB          0s elapsed", st2.GetStatusString())
+}
+
+func createResources(num int, t *testing.T) []Resource {
 	content := `abcdef`
 	port := test.TestHttpHandler(content, t)
 	resources := []Resource{}
 	algo := "sha256"
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < num; i++ {
 		resource := Resource{Urls: []string{fmt.Sprintf("http://localhost:%d/test%d.html", port, i)}, Integrity: fmt.Sprintf("%s-vvV+x/U6bUC+tkCngKY5yDvCmsipgW8fxsXG3Nk8RyE=", algo), Tags: []string{}, Filename: ""}
 		resources = append(resources, resource)
 	}
-	ctx, _ := context.WithCancel(context.Background())
 
-	st := NewStatusLine(ctx, &resources)
-
-	// Test InitResourceSizes.
-	err := st.InitResourcesSizes(1)
-	assert.NotNil(t, err)
-	err = st.InitResourcesSizes(1000)
-	assert.Nil(t, err)
-
-	// Test GetStatusString() and Increment().
-	st.Start(false)
-	assert.Equal(t, "\r-[                    ]          0/1000 Resources          0 B / 6.0 kB          0s elapsed", st.GetStatusString())
-	st.Increment(0)
-	time.Sleep(10 * time.Millisecond) // Give StatusLine loop time to update fields.
-	assert.Equal(t, "\r\\[                    ]          1/1000 Resources          6 B / 6.0 kB          0s elapsed", st.GetStatusString())
-	st.Increment(1)
-	time.Sleep(10 * time.Millisecond)
-	assert.Equal(t, "\r|[                    ]          2/1000 Resources          12 B / 6.0 kB          0s elapsed", st.GetStatusString())
-	st.Increment(2)
-	time.Sleep(10 * time.Millisecond)
-	assert.Equal(t, "\r/[                    ]          3/1000 Resources          18 B / 6.0 kB          0s elapsed", st.GetStatusString())
-	st.Increment(3)
-	time.Sleep(10 * time.Millisecond)
-	assert.Equal(t, "\r-[                    ]          4/1000 Resources          24 B / 6.0 kB          0s elapsed", st.GetStatusString())
+	return resources
 }
