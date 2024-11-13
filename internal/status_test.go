8c6147bde2210b5c203d45828849076205bfce66
Dylan Williams <mdylanwilliams576@gmail.com>
2024-11-12 23:38:39 -0500

    Fix test fail
    Adding a Sleep call in the Increment for loop in TestSLWith1000Resources seems to make the tests pass.
    
    The last Assert has been changed to a Contains to exclude the variable second counter.
    Each Sleep is now only 1 millisecond long instead of 10 -- this makes the test much faster while ensuring it still passes.
    
    Signed-off-by: Dylan Williams <mdylanwilliams576@gmail.com>

--- internal/status_test.go
+++ internal/status_test.go
@@ -21,10 +21,10 @@ func TestSLWith2Resources(t *testing.T) {
 	st1.Start(false)
 	assert.Equal(t, "\r-[            ]          0/2 Resources          0 B / 12 B          0s elapsed", st1.GetStatusString())
 	st1.Increment(0)
-	time.Sleep(10 * time.Millisecond) // Give SL loop time to update fields.
+	time.Sleep(1 * time.Millisecond) // Give SL loop time to update fields.
 	assert.Equal(t, "\r\\[██████      ]          1/2 Resources          6 B / 12 B          0s elapsed", st1.GetStatusString())
 	st1.Increment(1)
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	assert.Equal(t, "\r✔[████████████]          2/2 Resources          12 B / 12 B          0s elapsed", st1.GetStatusString())
 
 }
@@ -39,21 +39,22 @@ func TestSLWith1000Resources(t *testing.T) {
 	st2.Start(false)
 	assert.Equal(t, "\r-[                    ]          0/1000 Resources          0 B / 6.0 kB          0s elapsed", st2.GetStatusString())
 	st2.Increment(0)
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	assert.Equal(t, "\r\\[                    ]          1/1000 Resources          6 B / 6.0 kB          0s elapsed", st2.GetStatusString())
 	st2.Increment(1)
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	assert.Equal(t, "\r|[                    ]          2/1000 Resources          12 B / 6.0 kB          0s elapsed", st2.GetStatusString())
 	st2.Increment(2)
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	assert.Equal(t, "\r/[                    ]          3/1000 Resources          18 B / 6.0 kB          0s elapsed", st2.GetStatusString())
 	st2.Increment(3)
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	assert.Equal(t, "\r-[                    ]          4/1000 Resources          24 B / 6.0 kB          0s elapsed", st2.GetStatusString())
 	for i := 4; i < 1000; i++ {
 		st2.Increment(i)
+		time.Sleep(1 * time.Millisecond)
 	}
-	assert.Equal(t, "\r✔[████████████████████]          1000/1000 Resources          6.0 kB / 6.0 kB          0s elapsed", st2.GetStatusString())
+	assert.Contains(t, st2.GetStatusString(), "\r✔[████████████████████]          1000/1000 Resources          6.0 kB / 6.0 kB") // Test takes 1 or 2 seconds.
 }
 
 func TestSLWithCancelledContext(t *testing.T) {
@@ -67,7 +68,7 @@ func TestSLWithCancelledContext(t *testing.T) {
 	st.Start(false)
 	st.Increment(0)
 	cancel()
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	assert.NotEqual(t, true, st.isRunning.Load())
 }
 
